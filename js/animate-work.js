3165345290d8e2a8c962a5c63676d12e0ab622b5
Claudia <claudia.donea24@gmail.com>
2015-08-05 08:55:50 +0300

    additional changes

---
+++ js/animate-work.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+// $('.owl-carousel').owlCarousel({
+//     margin:0,
+//     center:true,
+//     autoWidth:true,
+//     merge: true,
+//     items:3
+// })
+
+	
+	$('.group-1').fadeIn(200).animate({width: "280px",left:"100px"},{duration:1000});
+
+	$('.group-3').fadeIn(200).delay(1200).animate({top:"100px"},{duration:1900});
+})
