24671f08769c3d3a7dc544f22f0503a10cbad5b5
Claudia <claudia.donea24@gmail.com>
2015-08-06 09:25:20 +0300

    added carousel

--- js/animate-work.js
+++ js/animate-work.js
@@ -1,28 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-// $('.owl-carousel').owlCarousel({
-//     margin:0,
-//     items:3,
-//     loop:true,
-//     navigation: true
-// })
+    // $('.owl-carousel').owlCarousel({
+    //     margin:0,
+    //     items:3,
+    //     loop:true,
+    //     navigation: true
+    // })
 
-$(document).ready(function () {
-    var carousel = $("#owl-demo");
-  carousel.owlCarousel({
-    navigation:true,
-    navigationText: [
-      "<i class='icon-chevron-left icon-white'><</i>",
-      "<i class='icon-chevron-right icon-white'>></i>"
-      ],
-  });
+    $(document).ready(function() {
+        var carousel = $("#owl-demo");
+        carousel.owlCarousel({
+            pagination: true,
+            singleItem: true,
+            height: 540,
+            autoWidth: true,
+            items: 3,
+            responsive: true,
+            navigationText: [
+                "<i class='icon-chevron-left icon-white'><</i>",
+                "<i class='icon-chevron-right icon-white'>></i>"
+            ]
+        });
 
-  
-});
+
+    });
 
 
-	
-	$('.group-1').fadeIn(200).animate({width: "280px",left:"100px"},{duration:1000});
 
-	$('.group-3').fadeIn(200).delay(1200).animate({top:"100px"},{duration:1900});
+    $('.group-1').fadeIn(200).animate({
+        width: "280px",
+        left: "100px"
+    }, {
+        duration: 1000
+    });
+
+    $('.group-3').fadeIn(200).delay(1200).animate({
+        top: "100px"
+    }, {
+        duration: 1900
+    });
 })
